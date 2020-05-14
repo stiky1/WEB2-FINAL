2e46f2a5c741acf8fabf675746954ec9b65291fe
zozoti3 <szitizoli@gmail.com>
2020-05-14 15:04:46 +0000

    adding navbar

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
-falat chuja
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="final_CSS.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Test Page - Fazy</title>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav flexing">
+            <li>
+                <a href="skLang.php"><i class="fas fa-cloud-sun"></i><br>Slovak Language</a>
+            </li>
+            <li>
+                <a href="index.php"><i class="fas fa-map-marked-alt"></i><br>English Language</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
-daj mi šalatu, daj mi šalatu.... a z jakej pici ked nemam zahradu
-hahah fanana
-sicke ste chuje
+<div id = "app">
+    <div class = "txt">
+        Under Testing<span class = "blink">_</span>
+    </div>
+</div>
+</body>
+</html>
---
+++ skLang.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang = "sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="final_CSS.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Testovacia Stránka - Fazy</title>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <ul class="nav navbar-nav flexing">
+            <li>
+                <a href="skLang.php"><i class="fas fa-cloud-sun"></i><br>Slovenský Jazyk</a>
+            </li>
+            <li>
+                <a href="index.php"><i class="fas fa-map-marked-alt"></i><br>Anglický Jazyk</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div id = "app">
+    <div class = "txt">
+        Pod testovaním<span class = "blink">_</span>
+    </div>
+</div>
+</body>
+</html>
