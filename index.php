0546047ffe0531b47927d5e2ab7504c04bfe230e
zozoti3 <szitizoli@gmail.com>
2020-05-16 18:33:26 +0000

    added plotly graph

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="final_CSS.css" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src='https://cdn.plot.ly/plotly-latest.min.js'></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <title>Test Page - Hungarian</title>
+    <title>Inverted Pendulum</title>
 </head>
 <body>
 <nav class="navbar navbar-inverse nav-improve">
@@ -23,16 +24,84 @@
         </ul>
     </div>
 </nav>
-<div id = "firstDiv">
-    <label for="const">Set value:</label>
+
+<div id = "firstDiv"><br>
+    <label for="const">Set new r position:</label>
     <input id="const" type="text">
-    <button>Send value</button><br><br>
-    <label for="exampleFormControlTextarea6">Set input:</label>
-    <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Write commands here..."></textarea>
+    <button>Send value</button><br><br><br>
+    <label for="exampleFormControlTextarea6">Select option:</label><br><br>
+    <label>
+        <input id = "graph" type="checkbox" alt="Graph" onclick="showMe()">
+    </label> Graph
+    <label>
+        <input id = "animation" type="checkbox" alt="Animation">
+    </label> Animation
+    <label>
+        <input id = "animation" type="checkbox" alt="Graph">
+    </label> Graph & Animation
 </div>
 <div id = "app">
+    <div id = "myDiv"></div>
+    <script>
+        function showMe() {
+            if(document.getElementById("graph").checked) {
+                document.getElementById("myDiv").style.display = "none";
+            } else {
+                document.getElementById("myDiv").style.display = "block";
+            }
+        }
+    </script>
     <div class = "txt">
-        Pendulum<span class = "blink">_</span>
+        <?php
+        $cmd = "octave -q --no-window-system --eval '[t,y]=kyvadlo()'";
+        exec($cmd,$op);
+
+        unset($op[0], $op[1]);
+        $i = 0;
+
+        for (;$i <= sizeof($op);$i++){
+            $key = array_search("",$op);
+            unset($op[$key]);
+        }
+
+        $foundT = false;
+        $foundY = false;
+        $t = array();
+        $y = array();
+
+        foreach ($op as $cell){
+            if($cell === 't =' || $foundT){
+                $foundT = true;
+                array_push($t,round($cell,2));
+            }
+            if ($cell === 'y =' || $foundY){
+                $foundT = false;
+                $foundY = true;
+                array_push($y,round($cell,5));
+            }
+        }
+        $key = array_search("t =",$t);
+        unset($t[$key]);
+        $key = array_search("y =",$y);
+        unset($y[$key]);
+        ?>
+        <div>
+            <embed src="svg/Ellipse%201.svg">
+        </div>
+        <script>
+            var xArray= [<?php echo '"'.implode('","', $t).'"' ?>];
+            var yArray= [<?php echo '"'.implode('","', $y).'"' ?>];
+
+            var trace1 = {
+              x: xArray,
+              y: yArray,
+              type: 'scatter'
+            };
+
+            var data = [trace1];
+
+            Plotly.newPlot('myDiv', data);
+        </script>
     </div>
 </div>
 </body>
